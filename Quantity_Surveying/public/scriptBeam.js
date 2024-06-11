ddc3233420453d7ab662d68fdb412b3ed15bf3c9
BitLess246 <raymval246@gmail.com>
2024-06-12 04:19:43 +0800

    fixed beam script

--- Quantity_Surveying/public/scriptBeam.js
+++ Quantity_Surveying/public/scriptBeam.js
@@ -1,34 +1,35 @@
+import { SaveFile } from './script.js';
 document.addEventListener("DOMContentLoaded", () => {
   let resContent;
   document.getElementById('formBeam').addEventListener('submit', function(event) {
     event.preventDefault();
     try {
+      console.log("1")
       let length = parseFloat(document.getElementById('length2').value);
       let width = parseFloat(document.getElementById('width2').value);
       let height = parseFloat(document.getElementById('height2').value);
       let numStructures = parseInt(document.getElementById('numStructures2').value);
       let concreteClass = document.getElementById('concreteClass2').value;
-
-
+      console.log("2")
       let topLengthPerPieceSupport = parseFloat(document.getElementById('topLengthPerPieceSupport').value);
       let numTopPiecesSupport = parseInt(document.getElementById('numTopPiecesSupport').value);
       let diameterTopSupport = parseFloat(document.getElementById('diameterTopSupport').value);
       let bottomLengthPerPieceSupport = parseFloat(document.getElementById('bottomLengthPerPieceSupport').value);
       let numBottomPiecesSupport = parseInt(document.getElementById('numBottomPiecesSupport').value);
       let diameterBottomSupport = parseFloat(document.getElementById('diameterBottomSupport').value);
-
+      console.log("3")
       let topLengthPerPieceMidspan = parseFloat(document.getElementById('topLengthPerPieceMidspan').value);
       let numTopPiecesMidspan = parseInt(document.getElementById('numTopPiecesMidspan').value);
       let diameterTopMidspan = parseFloat(document.getElementById('diameterTopMidspan').value);
       let bottomLengthPerPieceMidspan = parseFloat(document.getElementById('bottomLengthPerPieceMidspan').value);
       let numBottomPiecesMidspan = parseInt(document.getElementById('numBottomPiecesMidspan').value);
       let diameterBottomMidspan = parseFloat(document.getElementById('diameterBottomMidspan').value);
-                           
+      console.log("4")    
       let topSupNetLength = netLengthCalculation(topLengthPerPieceSupport,numTopPiecesSupport,numStructures);               
       let topMidNetLength = netLengthCalculation(topLengthPerPieceMidspan,numTopPiecesMidspan,numStructures);
       let botSupNetLength = netLengthCalculation(bottomLengthPerPieceSupport,numBottomPiecesSupport,numStructures);     
       let botMidNetLength = netLengthCalculation(bottomLengthPerPieceMidspan,numBottomPiecesMidspan,numStructures);
-        
+      console.log("5")
       let lengthPerSet = parseFloat(document.getElementById('lengthPerSet2').value);
       let noShearReinforcement = parseInt(document.getElementById('noShearReinforcement2').value);
       let lateralTieDiameter = parseFloat(document.getElementById('lateralTieDiameter2').value);
@@ -36,12 +37,13 @@ document.addEventListener("DOMContentLoaded", () => {
       let numIntersections = parseInt(document.getElementById('numIntersections2').value);
       let textSteel ="";
       let textSteel2 ="";
-        
-      if (diameterBottomMidspan === diameterBottomSupport === diameterTopMidspan === diameterTopSupport){ 
+      console.log("6")
+      if (diameterBottomMidspan === diameterBottomSupport || diameterBottomMidspan === diameterTopMidspan || diameterBottomMidspan === diameterTopSupport){ 
         //Assuming that all diameters are the same
-        let totalNetLength = topSupNetLength.netLength + topMidNetLength.netLength + botSupNetLength.netLength + botMidNetLength.netLength
-        let diameter = diameterTopSupport
-        let mainSteel = calculateSteelWeight(totalNetLength, diameter)
+        console.log("7")
+        let totalNetLength = (parseFloat(topSupNetLength.netLength) + parseFloat(topMidNetLength.netLength) + parseFloat(botSupNetLength.netLength) + parseFloat(botMidNetLength.netLength)).toFixed(2);
+        let diameter = diameterTopSupport;
+        let mainSteel = calculateSteelWeight(totalNetLength, diameter);
         textSteel = `
         <p>Net Length (Top Bars @ Support) = ${topSupNetLength.lengthPerPiece} * ${topSupNetLength.numBars} *${topSupNetLength.numStructures} = ${topSupNetLength.netLength} meters </p>
         <p>Net Length (Top Bars @ Midspan) = ${topMidNetLength.lengthPerPiece} * ${topMidNetLength.numBars} *${topMidNetLength.numStructures} = ${topMidNetLength.netLength} meters </p>
@@ -54,14 +56,16 @@ document.addEventListener("DOMContentLoaded", () => {
         textSteel2 = `
         <p>Net Length: ${mainSteel.netLength} meters</p>
         <p>Steel Weight (⌀${mainSteel.dia*1000}mm): ${mainSteel.steelWeight} kilograms</p>
-        `
+        `;
 
-        console.log("steel weight")
+        console.log("steel weight");
         } else {
-        let mainSteelTopSup = calculateSteelWeight(topSupNetLength.netLength, diameterTopSupport)
-        let mainSteelTopMid = calculateSteelWeight(topMidNetLength.netLength, diameterTopMidspan)
-        let mainSteelBotSup = calculateSteelWeight(botSupNetLength.netLength, diameterBottomSupport)
-        let mainSteelBotMid = calculateSteelWeight(botMidNetLength.netLength, diameterBottomMidspan)
+        console.log("8");
+        let mainSteelTopSup = calculateSteelWeight(topSupNetLength.netLength, diameterTopSupport);
+        let mainSteelTopMid = calculateSteelWeight(topMidNetLength.netLength, diameterTopMidspan);
+        let mainSteelBotSup = calculateSteelWeight(botSupNetLength.netLength, diameterBottomSupport);
+        let mainSteelBotMid = calculateSteelWeight(botMidNetLength.netLength, diameterBottomMidspan);
+        console.log("9");
         textSteel = `
         <p>Net Length (Top Bars @ Support) = ${topSupNetLength.lengthPerPiece} * ${topSupNetLength.numBars} *${topSupNetLength.numStructures} = ${topSupNetLength.netLength} meters </p>
         <p>Net Length (Top Bars @ Midspan) = ${topMidNetLength.lengthPerPiece} * ${topMidNetLength.numBars} *${topMidNetLength.numStructures} = ${topMidNetLength.netLength} meters </p>
@@ -79,7 +83,8 @@ document.addEventListener("DOMContentLoaded", () => {
         <p>Steel Weight (Top Bars @ Midspan, ⌀${mainSteelTopMid.dia*1000}mm) = ${mainSteelTopMid.noOfPcs} * 6 * ${mainSteelTopMid.area.toFixed(6)} * 7850 = ${mainSteelTopMid.steelWeight} kilograms</p>
         <p>Steel Weight (Bottom Bars @ Support, ⌀${mainSteelBotSup.dia*1000}mm) = ${mainSteelBotSup.noOfPcs} * 6 * ${mainSteelBotSup.area.toFixed(6)} * 7850 = ${mainSteelBotSup.steelWeight} kilograms</p>
         <p>Steel Weight (Bottom Bars @ Midspan, ⌀${mainSteelBotMid.dia*1000}mm) = ${mainSteelBotMid.noOfPcs} * 6 * ${mainSteelBotMid.area.toFixed(6)} * 7850 = ${mainSteelBotMid.steelWeight} kilograms</p>
-        `
+        `;
+        console.log("10");
         textSteel2 =`
         <p>Net Length (Top Bars @ Support) =  ${topSupNetLength.netLength} meters </p>
         <p>Net Length (Top Bars @ Midspan) =  ${topMidNetLength.netLength} meters </p>
@@ -89,24 +94,23 @@ document.addEventListener("DOMContentLoaded", () => {
         <p>Steel Weight (Top Bars @ Midspan, ⌀${mainSteelTopMid.dia*1000}mm) = ${mainSteelTopMid.steelWeight} kilograms</p>
         <p>Steel Weight (Bottom Bars @ Support, ⌀${mainSteelBotSup.dia*1000}mm) = ${mainSteelBotSup.steelWeight} kilograms</p>
         <p>Steel Weight (Bottom Bars @ Midspan, ⌀${mainSteelBotMid.dia*1000}mm) = ${mainSteelBotMid.steelWeight} kilograms</p>
-        `
-        console.log("steel weight")
+        `;
+        console.log("steel weight");
         }
         
+        console.log("11");
+        let volumeConc= calculateConcreteVolume(length,width,height,numStructures);
+        console.log("volume");
+        let materials = calculateConcreteMaterials(volumeConc.volume,concreteClass);
+        console.log("conc materials");
         
-        let volumeConc= calculateConcreteVolume(length,width,height,numStructures)
-        console.log("volume")
-        let materials = calculateConcreteMaterials(volume,concreteClass)
-        console.log("conc materials")
-        
-        let reinforcementSteel = calculateLateralTieWeight (lengthPerSet, noShearReinforcement, lateralTieDiameter,numStructures)
-        console.log("reinf steel weight")
-        let tieWire = calculateTieWire(lengthPerCut, numIntersections, numStructures)
-        console.log("tie wire")
+        let reinforcementSteel = calculateLateralTieWeight (lengthPerSet, noShearReinforcement, lateralTieDiameter,numStructures);
+        console.log("reinf steel weight");
+        let tieWire = calculateTieWire(lengthPerCut, numIntersections, numStructures);
+        console.log("tie wire");
       
         // Call the displayResults function
         
-        document.getElementById('saveButton').style.display = 'block';
         const results = displayResults(volumeConc, materials, reinforcementSteel, tieWire, textSteel, textSteel2);
         resContent = results.innerText;
         console.log(results);
@@ -119,46 +123,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 const saveButtonElement = document.getElementById("saveButton");
-saveButtonElement.addEventListener("click", function() {
-  let defaultFileName = "file.txt"
-  // Prompt the user for a filename
-  let fileName = window.prompt("Enter a filename:", defaultFileName);
-  // If the user cancels or enters an empty filename, do nothing
-  if (!fileName) return;
-  //downloadTextFile(resultDiv, fileName)
-  downloadTextFile(resContent, fileName)
+saveButtonElement.addEventListener("click", function(){
+  SaveFile(resContent);
 });
 
-function downloadTextFile(content, fileName){
-// fetch('/download',{
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'text/plain'
-//   },
-//   body: JSON.stringify({ text: content})
-// })
-// .then(response => response.blob())
-// .then(blob => {
-//   const url = window.URL.createObjectURL(blob);
-//   const a = document.createElement('a');
-//   a.style.display = 'none';
-//   a.href = url;
-//   a.download = fileName;
-//   document.body.appendChild(a);
-//   a.click();
-//   window.URL.revokeObjectURL(url);
-// })
-// .catch(error => console.error('Error:', error));
-const blob = new Blob([content], { type: 'text/plain' });
-const url = window.URL.createObjectURL(blob);
-const a = document.createElement('a');
-a.style.display = 'none';
-a.href = url;
-a.download = fileName;
-document.body.appendChild(a);
-a.click();
-window.URL.revokeObjectURL(url);
-}
 
 
 
@@ -166,7 +134,7 @@ window.URL.revokeObjectURL(url);
 
 
 function calculateConcreteVolume(length, width, height, numStructures) {
-  volume = (length * width * height * numStructures).toFixed(2) 
+  const volume = (length * width * height * numStructures).toFixed(2) 
   return {volume , length, width, height};
 }
 
@@ -194,17 +162,17 @@ function calculateConcreteMaterials(volumeInCubicMeters, concreteClass) {
 }
 
 function calculateSteelWeight(netLength, dia) {
-  area = (((Math.PI)/4)*dia**2)
-  noOfPcs= Math.ceil(netLength/5.6) 
-  steelWeight = (noOfPcs * 6 * area * 7850).toFixed(2)
-  return {steelWeight, area, netLength, dia, noOfPcs, };
+  let area = (((Math.PI)/4)*dia**2)
+  let noOfPcs= Math.ceil(netLength/5.6) 
+  let steelWeight = (noOfPcs * 6 * area * 7850).toFixed(2)
+  return {steelWeight, area, netLength, dia, noOfPcs };
 }
 
 function calculateLateralTieWeight(lengthPerSet, noShearReinforcement, lateralTieDiameter, numStructures) {
-  netLength = (lengthPerSet * noShearReinforcement  * numStructures).toFixed(2)
-  area = ((Math.PI)/4)*lateralTieDiameter**2
-  noOfPcs= Math.ceil(netLength/5.6) 
-  steelWeight = (noOfPcs * 6 * area * 7850).toFixed(2);
+  const netLength = (lengthPerSet * noShearReinforcement  * numStructures).toFixed(2)
+  const area = ((Math.PI)/4)*lateralTieDiameter**2
+  const noOfPcs= Math.ceil(netLength/5.6) 
+  const steelWeight = (noOfPcs * 6 * area * 7850).toFixed(2);
   return {steelWeight, area, netLength, lateralTieDiameter, noOfPcs, lengthPerSet, noShearReinforcement, numStructures };
 }
 
@@ -216,6 +184,9 @@ function calculateTieWire(lengthPerCut, numIntersections, numStructures) {
 }  
 
 function netLengthCalculation(lengthPerPiece, numBars, numStructures) {
+  lengthPerPiece = parseFloat(lengthPerPiece);
+  numBars = parseFloat(numBars);
+  numStructures = parseFloat(numStructures);
   const netLength = (lengthPerPiece * numBars * numStructures).toFixed(2)
   return {netLength,lengthPerPiece, numBars, numStructures};
 }
