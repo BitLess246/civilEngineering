ba493517858615f55601c315d8bded85d435eec3
BitLess246 <raymval246@gmail.com>
2024-06-13 00:13:27 +0800

    fixed some equations

--- Quantity_Surveying/public/scriptBeam.js
+++ Quantity_Surveying/public/scriptBeam.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
       let height = parseFloat(document.getElementById('height2').value);
       let numStructures = parseInt(document.getElementById('numStructures2').value);
       let concreteClass = document.getElementById('concreteClass2').value;
+      let spliceLength = parseFloat(document.getElementById('lengthPerSplice').value);
       console.log("2")
       let topLengthPerPieceSupport = parseFloat(document.getElementById('topLengthPerPieceSupport').value);
       let numTopPiecesSupport = parseInt(document.getElementById('numTopPiecesSupport').value);
@@ -43,7 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("7")
         let totalNetLength = (parseFloat(topSupNetLength.netLength) + parseFloat(topMidNetLength.netLength) + parseFloat(botSupNetLength.netLength) + parseFloat(botMidNetLength.netLength)).toFixed(2);
         let diameter = diameterTopSupport;
-        let mainSteel = calculateSteelWeight(totalNetLength, diameter);
+        let mainSteel = calculateSteelWeight(totalNetLength, diameter, spliceLength);
         textSteel = `
         <p>Net Length (Top Bars @ Support) = ${topSupNetLength.lengthPerPiece} * ${topSupNetLength.numBars} *${topSupNetLength.numStructures} = ${topSupNetLength.netLength} meters </p>
         <p>Net Length (Top Bars @ Midspan) = ${topMidNetLength.lengthPerPiece} * ${topMidNetLength.numBars} *${topMidNetLength.numStructures} = ${topMidNetLength.netLength} meters </p>
@@ -51,20 +52,20 @@ document.addEventListener("DOMContentLoaded", () => {
         <p>Net Length (Bottom Bars @ Midspan) = ${botMidNetLength.lengthPerPiece} * ${botMidNetLength.numBars} *${botMidNetLength.numStructures} = ${botMidNetLength.netLength} meters </p>
         <p>Total Net Length = ${topSupNetLength.netLength} + ${topMidNetLength.netLength} + ${botSupNetLength.netLength} + ${botMidNetLength.netLength} = ${totalNetLength} meters</p>
         <p>Area = (π/4) * ${mainSteel.dia}^2 = ${mainSteel.area.toFixed(6)} square meters</p>
-        <p>No. of Bars = ${mainSteel.netLength} / 5.6 ≈ ${mainSteel.noOfPcs} pieces</p>
+        <p>No. of Bars = ${mainSteel.netLength} / ${mainSteel.splice} ≈ ${mainSteel.noOfPcs} pieces</p>
         <p>Steel Weight = ${mainSteel.noOfPcs} * 6 * ${mainSteel.area.toFixed(6)} * 7850 = ${mainSteel.steelWeight} kilograms</p>`
         textSteel2 = `
-        <p>Net Length: ${mainSteel.netLength} meters</p>
+        <p>No. of Bars: ${mainSteel.noOfPcs} pieces</p>
         <p>Steel Weight (⌀${mainSteel.dia*1000}mm): ${mainSteel.steelWeight} kilograms</p>
         `;
 
         console.log("steel weight");
         } else {
         console.log("8");
-        let mainSteelTopSup = calculateSteelWeight(topSupNetLength.netLength, diameterTopSupport);
-        let mainSteelTopMid = calculateSteelWeight(topMidNetLength.netLength, diameterTopMidspan);
-        let mainSteelBotSup = calculateSteelWeight(botSupNetLength.netLength, diameterBottomSupport);
-        let mainSteelBotMid = calculateSteelWeight(botMidNetLength.netLength, diameterBottomMidspan);
+        let mainSteelTopSup = calculateSteelWeight(topSupNetLength.netLength, diameterTopSupport, spliceLength);
+        let mainSteelTopMid = calculateSteelWeight(topMidNetLength.netLength, diameterTopMidspan, spliceLength);
+        let mainSteelBotSup = calculateSteelWeight(botSupNetLength.netLength, diameterBottomSupport, spliceLength);
+        let mainSteelBotMid = calculateSteelWeight(botMidNetLength.netLength, diameterBottomMidspan, spliceLength);
         console.log("9");
         textSteel = `
         <p>Net Length (Top Bars @ Support) = ${topSupNetLength.lengthPerPiece} * ${topSupNetLength.numBars} *${topSupNetLength.numStructures} = ${topSupNetLength.netLength} meters </p>
@@ -75,10 +76,10 @@ document.addEventListener("DOMContentLoaded", () => {
         <p>Area (Top Bars @ Midspan) = (π/4) * ${mainSteelTopMid.dia}^2 = ${mainSteelTopMid.area.toFixed(6)} square meters</p>
         <p>Area (Bottom Bars @ Support) = (π/4) * ${mainSteelBotSup.dia}^2 = ${mainSteelBotSup.area.toFixed(6)} square meters</p>
         <p>Area (Bottom Bars @ Midspan) = (π/4) * ${mainSteelBotMid.dia}^2 = ${mainSteelBotMid.area.toFixed(6)} square meters</p>
-        <p>No. of Bars (Top Bars @ Support) = ${mainSteelTopSup.netLength} / 5.6 ≈ ${mainSteelTopSup.noOfPcs} pieces</p>
-        <p>No. of Bars (Top Bars @ Midspan) = ${mainSteelTopMid.netLength} / 5.6 ≈ ${mainSteelTopMid.noOfPcs} pieces</p>
-        <p>No. of Bars (Bottom Bars @ Support) = ${mainSteelBotSup.netLength} / 5.6 ≈ ${mainSteelBotSup.noOfPcs} pieces</p>
-        <p>No. of Bars (Bottom Bars @ Midspan) = ${mainSteelBotMid.netLength} / 5.6 ≈ ${mainSteelBotMid.noOfPcs} pieces</p>
+        <p>No. of Bars (Top Bars @ Support) = ${mainSteelTopSup.netLength} / ${mainSteelTopSup.splice} ≈ ${mainSteelTopSup.noOfPcs} pieces</p>
+        <p>No. of Bars (Top Bars @ Midspan) = ${mainSteelTopMid.netLength} / ${mainSteelTopMid.splice} ≈ ${mainSteelTopMid.noOfPcs} pieces</p>
+        <p>No. of Bars (Bottom Bars @ Support) = ${mainSteelBotSup.netLength} / ${mainSteelBotSup.splice} ≈ ${mainSteelBotSup.noOfPcs} pieces</p>
+        <p>No. of Bars (Bottom Bars @ Midspan) = ${mainSteelBotMid.netLength} / ${mainSteelBotMid.splice} ≈ ${mainSteelBotMid.noOfPcs} pieces</p>
         <p>Steel Weight (Top Bars @ Support, ⌀${mainSteelTopSup.dia*1000}mm) = ${mainSteelTopSup.noOfPcs} * 6 * ${mainSteelTopSup.area.toFixed(6)} * 7850 = ${mainSteelTopSup.steelWeight} kilograms</p>
         <p>Steel Weight (Top Bars @ Midspan, ⌀${mainSteelTopMid.dia*1000}mm) = ${mainSteelTopMid.noOfPcs} * 6 * ${mainSteelTopMid.area.toFixed(6)} * 7850 = ${mainSteelTopMid.steelWeight} kilograms</p>
         <p>Steel Weight (Bottom Bars @ Support, ⌀${mainSteelBotSup.dia*1000}mm) = ${mainSteelBotSup.noOfPcs} * 6 * ${mainSteelBotSup.area.toFixed(6)} * 7850 = ${mainSteelBotSup.steelWeight} kilograms</p>
@@ -161,19 +162,21 @@ function calculateConcreteMaterials(volumeInCubicMeters, concreteClass) {
   return {cement, sand, gravel, factorOfCement};
 }
 
-function calculateSteelWeight(netLength, dia) {
-  let area = (((Math.PI)/4)*dia**2)
-  let noOfPcs= Math.ceil(netLength/5.6) 
-  let steelWeight = (noOfPcs * 6 * area * 7850).toFixed(2)
-  return {steelWeight, area, netLength, dia, noOfPcs };
+function calculateSteelWeight(netLength, dia, spliceLength) {
+  let area = (((Math.PI)/4)*dia**2);
+  let splice = 6 - spliceLength;
+  let noOfPcs= Math.ceil(netLength/splice);
+  let steelWeight = (noOfPcs * 6 * area * 7850).toFixed(2);
+  return {steelWeight, area, netLength, dia, noOfPcs, splice };
 }
 
 function calculateLateralTieWeight(lengthPerSet, noShearReinforcement, lateralTieDiameter, numStructures) {
-  const netLength = (lengthPerSet * noShearReinforcement  * numStructures).toFixed(2)
-  const area = ((Math.PI)/4)*lateralTieDiameter**2
-  const noOfPcs= Math.ceil(netLength/5.6) 
+  const cutsPer6m = Math.floor(6/lengthPerSet);
+  const totalNoShearReinforcement = noShearReinforcement * numStructures;
+  let area = (Math.PI/4) * lateralTieDiameter ** 2;
+  const noOfPcs= Math.ceil(totalNoShearReinforcement/cutsPer6m) ;
   const steelWeight = (noOfPcs * 6 * area * 7850).toFixed(2);
-  return {steelWeight, area, netLength, lateralTieDiameter, noOfPcs, lengthPerSet, noShearReinforcement, numStructures };
+  return {cutsPer6m, totalNoShearReinforcement,steelWeight, area, lateralTieDiameter, noOfPcs, lengthPerSet, noShearReinforcement, numStructures };
 }
 
 
@@ -208,10 +211,11 @@ function displayResults(volumeConc, materials, reinforcementSteel, tieWire, text
       <li><h5>Steel Weight Calculation</h5></li>
         <p><h5>Main Reinforcements:</h5></p>
         ${text1}
-        <p><h5>Lateral Ties:</h5></p>
-        <p>Net length = ${reinforcementSteel.lengthPerSet} * ${reinforcementSteel.noShearReinforcement} *${reinforcementSteel.numStructures} = ${reinforcementSteel.netLength} meters </p>
+        <p><h5>Stirrups:</h5></p>
+        <p>No. of Cuts per 6m = 6 / ${reinforcementSteel.lengthPerSet} ≈ ${reinforcementSteel.cutsPer6m} pieces </p>
+        <p>Total No. of Stirrups = ${reinforcementSteel.noShearReinforcement} * ${reinforcementSteel.numStructures} = ${reinforcementSteel.totalNoShearReinforcement} pieces</p>
         <p>Area = (π/4) * ${reinforcementSteel.lateralTieDiameter}^2 = ${reinforcementSteel.area.toFixed(6)} square meters</p>
-        <p>No. of Bars = ${reinforcementSteel.netLength} / 5.6 ≈ ${reinforcementSteel.noOfPcs} pieces</p>
+        <p>No. of Bars = ${reinforcementSteel.totalNoShearReinforcement} / ${reinforcementSteel.cutsPer6m} ≈ ${reinforcementSteel.noOfPcs} pieces</p>
         <p>Steel Weight = ${reinforcementSteel.noOfPcs} * 6 * ${reinforcementSteel.area.toFixed(6)} * 7850 = ${reinforcementSteel.steelWeight} kilograms</p>
       <li><h5>G.I. Tie Wire Calculation</h5></li>
         <p>Total Length = length per cut * number of intersections * number of structures</p>
@@ -231,8 +235,8 @@ function displayResults(volumeConc, materials, reinforcementSteel, tieWire, text
     <li><h5>Steel Weight:</h5></li>
     <p><h5>Main Reinforcements:</h5></p>
     ${text2}
-    <p><h5>Lateral Ties:</h5></p>
-    <p>Net Length: ${reinforcementSteel.netLength} meters</p>
+    <p><h5>Stirrups:</h5></p>
+    <p>No. of Bars: ${reinforcementSteel.noOfPcs} pieces</p>
     <p>Area: ${reinforcementSteel.area.toFixed(6)} square meters</p>
     <p>Steel Weight (⌀${reinforcementSteel.lateralTieDiameter*1000}mm): ${reinforcementSteel.steelWeight} kilograms</p>
     <li><h5>Tie Wire:</h5></li>
